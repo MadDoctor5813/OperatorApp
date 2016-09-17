eeff089ffd74f598fd53439476c62ce07d34ec50
betato <battlestargatticus@gmail.com>
2016-09-17 13:52:22 -0400

    Add dll interface methods

--- Operator/ImageEncoder/Class1.cs
+++ Operator/ImageEncoder/Class1.cs
@@ -7,6 +7,53 @@ namespace ImageEncoder
 {
     public class BitmapToJpeg
     {
-        
+        public const long DefaultJpegQualityLevel = 75L;
+        public const int DefaultInputPixelLimit = 1280 * 720;
+
+        private long _jpegQualityLevel = DefaultJpegQualityLevel;
+        private int _inputPixelLimit = DefaultInputPixelLimit;
+
+        public long JpegQualityLevel
+        {
+            get
+            {
+                return _jpegQualityLevel;
+            }
+            set
+            {
+                if (0 < value && value < 100)
+                {
+                    _jpegQualityLevel = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        public int InputPixelLimit
+        {
+            get
+            {
+                return _inputPixelLimit;
+            }
+            set
+            {
+                if (value > 1)
+                {
+                    _inputPixelLimit = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        public static byte[] ConvertToJpeg(byte[] bitmap)
+        {
+            return new byte[0];
+        }
     }
 }
