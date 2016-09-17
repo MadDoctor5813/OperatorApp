25fc73adb2f24c0c951e0d6444fffbe0fb3ef5d6
betato <battlestargatticus@gmail.com>
2016-09-17 18:47:43 -0400

    Finish LocationFragment

--- Operator/Operator/Resources/LocationFragment.cs
+++ Operator/Operator/Resources/LocationFragment.cs
@@ -12,6 +12,7 @@ using Android.Views;
 using Android.Widget;
 using Android.Gms.Maps;
 using Android.Gms.Maps.Model;
+using System.Net;
 
 namespace Operator.Resources
 {
@@ -40,9 +41,15 @@ namespace Operator.Resources
             }
         }
 
+        private Button submitButton;
+        private Button photoSubmitButton;
+
+        private TextView detailsField;
+
         public LocationFragment(SubmitActivity submitActivity)
         {
             this.submitActivity = submitActivity;
+            submitActivity.LocationFragment = this;
         }
 
         public override void OnCreate(Bundle savedInstanceState)
@@ -58,18 +65,58 @@ namespace Operator.Resources
         public override void OnActivityCreated(Bundle savedInstanceState)
         {
             base.OnActivityCreated(savedInstanceState);
+            initMap();
             locationOptions = Activity.FindViewById<RadioGroup>(Resource.Id.LocationOptions);
             locationOptions.SetOnCheckedChangeListener(this);
             Activity.FindViewById<Button>(Resource.Id.backButton).Click += BackButton_Click;
+            submitButton = Activity.FindViewById<Button>(Resource.Id.submitButton);
+            submitButton.Click += SubmitButton_Click;
+            photoSubmitButton = Activity.FindViewById<Button>(Resource.Id.PhotoSubmit);
+            photoSubmitButton.Click += PhotoSubmitButton_Click;
 
-            initMap();
+            detailsField = Activity.FindViewById<TextView>(Resource.Id.Details);
         }
 
         private void initMap()
         {
             mapFrag = new MapFragment();
             FragmentManager.BeginTransaction().Add(Resource.Id.MapFrame, mapFrag).Commit();
-            mapFrag.GetMapAsync(this);
+            mapFrag.GetMapAsync(this);           
+        }
+
+        private void PhotoSubmitButton_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            EmergencySubmission emergency = new EmergencySubmission();
+            emergency.Category = submitActivity.TypeFragment.EmergencyType;
+            emergency.Details = detailsField.Text;
+            string id = null;
+            try
+            {
+                id = ServerHelper.SubmitEmergency(emergency);
+            }
+            catch (Exception ex) when (ex is WebException || ex is Java.IO.IOException)
+            {
+                ServerHelper.ShowErrorDialog(Activity, "Submit Error", "Error submitting emergency. Please retry.");
+            }
+            if (!TrackLocation)
+            {
+                GeocodedLocation loc = new GeocodedLocation();
+                loc.Latitude = (float)(SelectedLoc.Latitude);
+                loc.Longitude = (float)(SelectedLoc.Longitude);
+                try
+                {
+                    ServerHelper.SubmitLocation(loc, id, Activity);
+                }
+                catch (Exception ex) when (ex is WebException || ex is Java.IO.IOException)
+                {
+                    ServerHelper.ShowErrorDialog(Activity, "Submit Error", "Error submitting emergency. Please retry.");
+                }
+            }
         }
 
         private void BackButton_Click(object sender, EventArgs e)
