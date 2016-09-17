07b236e4ef2e4ee21229450c518938f7f3ea86e9
betato <battlestargatticus@gmail.com>
2016-09-17 17:03:38 -0400

    Add ServerHelper to manage sending and recieving

---
+++ Operator/Operator/Resources/ServerHelper.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Net;
+using Newtonsoft.Json;
+using Android.Locations;
+
+namespace Operator.Resources
+{
+    class ServerHelper
+    {
+        private static WebClient webClient = new WebClient();
+        private static readonly string ServerUrl = "operatorapp.ca";
+
+        public static string SubmitEmergency(EmergencySubmission emergency)
+        {
+            string data = JsonConvert.SerializeObject(emergency);
+            byte[] response = webClient.UploadData(ServerUrl + "/insertEmergencyJSON", Encoding.UTF8.GetBytes(data));
+            return Encoding.UTF8.GetString(response);
+        }
+
+        public static void SubmitLocation(GeocodedLocation location, string id, Context context)
+        {
+            //fill in geocoding data
+            Geocoder geocoder = new Geocoder(context);
+            Address address = geocoder.GetFromLocation(location.Longitude, location.Latitude, 1)[0];
+            location.PostalCode = address.PostalCode;
+            location.Province = address.AdminArea;
+            location.Street = address.Thoroughfare;
+            location.City = address.Locality;
+            string data = JsonConvert.SerializeObject(location);
+            byte[] response = webClient.UploadData(ServerUrl + "/updateLocationJSON/" + id, Encoding.UTF8.GetBytes(data)); 
+        }
+
+        public static ActiveEmergency GetActiveEmergency(string id)
+        {
+            byte[] response = webClient.DownloadData(ServerUrl + "/loadEmergencyJSON/" + id);
+            return JsonConvert.DeserializeObject<ActiveEmergency>(Encoding.UTF8.GetString(response));
+        }
+
+        public static void uploadImage(byte[] imageData, string filename, string[] labels)
+        {
+            webClient.UploadData(ServerUrl + "/uploadImage/" + filename, imageData);
+        }
+    }
+}
