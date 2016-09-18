5bece7344dbd2746da0dedc8ace85045aa7b5d62
betato <battlestargatticus@gmail.com>
2016-09-18 02:32:02 -0400

    Finish EndActivity

--- Operator/Operator/Resources/EndActivity.cs
+++ Operator/Operator/Resources/EndActivity.cs
@@ -21,7 +21,23 @@ namespace Operator.Resources
             
             SetContentView(Resource.Layout.EndLayout);
             FindViewById<Button>(Resource.Id.exitButton).Click += EndActivity_Click;
-            
+
+            int stat = Intent.Extras.GetInt("FinalStatus"); // Use to display the status
+            string statStr = null;
+
+            switch (stat)
+            {
+                case 3:
+                    statStr = "complete";
+                    break;
+                case 4:
+                    statStr = "trash";
+                    break;
+                case 5:
+                    statStr = "archived";
+                    break;
+            }
+            FindViewById<TextView>(Resource.Id.EndText).Text = string.Format(FindViewById<TextView>(Resource.Id.EndText).Text, statStr);
         }
 
         private void EndActivity_Click(object sender, EventArgs e)
